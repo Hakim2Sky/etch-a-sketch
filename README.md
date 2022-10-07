ed78341ccad16f3da2389bc2bfd0e86de85491cb
Hakim2Sky <m.hakim@outlook.com>
2022-10-07 23:18:58 +0200

    create grid function finished

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # etch-a-sketch
+create grid of square divs 16 * 16 960px space
+    inside container div
+    borders and margins can adjest the size
+2- hover effect over the grid so it change color when mouse pass over them
+    add event listener hovering
+    to change color 
+        1- add new class to the div
+        2- change the div's background color
+3- add button to top of the page
+    prompt user for input when entered:
+        remove existing grid
+        new grid take same total space
+        limit to max 100
+    
--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const squareDivs = [];
+let size = 16;
 const container = document.querySelector(".container");
 
 // create 16 * 16 divs Grid
-for (let i = 0; i < 256; i++) {
-    squareDivs[i] = document.createElement("div");
-    container.appendChild(squareDivs[i]);
-    console.log(squareDivs[i]);
+function createGrid(size) {
+    for (let i = 0; i < size ** 2; i++) {
+        squareDivs[i] = document.createElement("div");
+        container.appendChild(squareDivs[i]);
+    }
 }
+
+function sketch() {
+    size = parseInt(prompt("pelase enter your desired sketch size", "16"));
+    // remove the grid
+    squareDivs.forEach(div => div.remove())
+    // create new grid
+    createGrid(size);
+}
+
+createGrid(size);
+// add hovering effect
+squareDivs.forEach(div => div.addEventListener("mouseover", () => div.style.backgroundColor = "red"));
